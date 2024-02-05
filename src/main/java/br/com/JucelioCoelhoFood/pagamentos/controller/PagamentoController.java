53f53b5fe102825286961b86eddc1b5b8cdc4a2a
Jucelio coelho <juceliocoelho2022@gmail.com>
2024-02-05 17:29:15 -0300

    Atualização

--- src/main/java/br/com/JucelioCoelhoFood/pagamentos/controller/PagamentoController.java
+++ src/main/java/br/com/JucelioCoelhoFood/pagamentos/controller/PagamentoController.java
@@ -4,11 +4,11 @@ import br.com.JucelioCoelhoFood.pagamentos.dto.PagamentoDto;
 import br.com.JucelioCoelhoFood.pagamentos.service.PagamentoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import java.util.List;
 
@@ -28,4 +28,22 @@ public class PagamentoController {
        PagamentoDto dto = service.getByID(id);
        return ResponseEntity.ok(dto);
  }
+    @PostMapping
+    public ResponseEntity<PagamentoDto> cadastrar(@RequestBody @Valid PagamentoDto dto, UriComponentsBuilder uriBuilder){
+        PagamentoDto pagamento = service.createPagamento(dto);
+       var uri = uriBuilder.path("/pagamentos/{id}").buildAndExpand(pagamento.getId()).toUri();
+        return ResponseEntity.created(uri).body(pagamento);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<PagamentoDto> atualizarRegistro(@PathVariable @NotNull Long id,
+                                                          @RequestBody @Valid PagamentoDto dto){
+        PagamentoDto pagamentoAtualizado;
+        pagamentoAtualizado = service.updatePagamento(id, dto);
+        return  ResponseEntity.ok(pagamentoAtualizado);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<PagamentoDto> deletarRegistro(@PathVariable @NotNull Long id){
+        service.deletePagamento(id);
+        return ResponseEntity.noContent().build();
+    }
 }
